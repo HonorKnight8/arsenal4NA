3a11ff49496ff25a4f1e5fe8d29f0e9b2dcdb7a7
honorknight8 <Honor_Knight8@outlook.com>
2020-02-18 00:10:49 +0800

    20200217c
    1、完成“修改密码”功能的前端部分
    2、完成验证码类
    3、解决同一个页面，多个验证码问题

--- _preferences/preferencespersonal.class.php
+++ _preferences/preferencespersonal.class.php
@@ -32,22 +32,24 @@ class PreferencesPersonal extends Preferences
         //头像照片设置
         $this->div_preferences .= '<form action="" method="post" enctype="multipart/form-data">';
         $this->div_preferences .= '<fieldset><legend>头像照片设置</legend>';
-        $this->div_preferences .= '&emsp;&emsp;&emsp;<input type="file" name="file" id="file" placeholder="选择要上传的照片" accept="image/jpeg, image/gif, image/png, image/webp, image/bmp" /><br />';
-        $this->div_preferences .= '注意：支持的文件格式有：jpg、gif、png、webp、bmp<br />';
-        $this->div_preferences .= '&emsp;&emsp;&emsp;建议照片的横竖比例等于或接近于：1:1.45(证件照比例)<br />';
-        $this->div_preferences .= '&emsp;&emsp;&emsp;照片大小不得大于1MB<br />';
-        $this->div_preferences .= '&emsp;&emsp;&emsp;<input type="radio" name="showHeadPhoto" value="1"';
+        $this->div_preferences .= '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<input type="file" name="file" id="file" placeholder="选择要上传的照片" accept="image/jpeg, image/gif, image/png, image/webp, image/bmp" /><br />';
+        $this->div_preferences .= '&emsp;&emsp;&emsp;注意：支持的文件格式有：jpg、gif、png、webp、bmp<br />';
+        $this->div_preferences .= '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;建议照片的横竖比例等于或接近于：1:1.45(证件照比例)<br />';
+        $this->div_preferences .= '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;照片大小不得大于1MB<br />';
+        $this->div_preferences .= '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<input type="radio" name="showHeadPhoto" value="1"';
         if ($staffInfoArray['showHeadPhoto'] == 1) {
             $this->div_preferences .= 'checked';
         }
         $this->div_preferences .= '>显示照片';
-        $this->div_preferences .= '&emsp;&emsp;&emsp;<input type="radio" name="showHeadPhoto" value="0"';
+        $this->div_preferences .= '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<input type="radio" name="showHeadPhoto" value="0"';
         if ($staffInfoArray['showHeadPhoto'] == 0) {
             $this->div_preferences .= 'checked';
         }
         $this->div_preferences .= '>隐藏照片<br />';
-        $this->div_preferences .= '注意：显示/隐藏照片选项，对全员生效<br />';
-        $this->div_preferences .= '&emsp;&emsp;&emsp;<input type="submit" name="PreferencesPersonal_1" value="上传" />';
+        $this->div_preferences .= '&emsp;&emsp;&emsp;注意：显示/隐藏照片选项，对全员生效<br />';
+        $this->div_preferences .= '输入验证码：<input type="text" name="inputcaptchaget" id="" placeholder="请输入右侧的验证码……">';
+        $this->div_preferences .= '<img src="" alt="" id="verifyimage" /><a onclick="document.getElementById(\'verifyimage\').src=\'_libs/captchaget0.php?r=\'+Math.random()" href="javascript:void(0)">显示/更换验证码</a><br />';
+        $this->div_preferences .= '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<input type="submit" name="PreferencesPersonal_1" value="上传" />';
         $this->div_preferences .= '</fieldset></form>';
 
         //修改密码
@@ -58,6 +60,8 @@ class PreferencesPersonal extends Preferences
         $this->div_preferences .= '确认新密码：<input type="password" name="password" id="" style="width:250px" placeholder="请再一次输入新密码..." required><br />';
         $this->div_preferences .= '&emsp;&emsp;&emsp;注意：密码长度最小8位，最大24位位<br />';
         $this->div_preferences .= '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;密码只能包含数字、大小写字母<br />';
+        $this->div_preferences .= '输入验证码：<input type="text" name="inputcaptchaget" id="" placeholder="请输入右侧的验证码……">';
+        $this->div_preferences .= '<img src="" alt="" id="verifyimage1" /><a onclick="document.getElementById(\'verifyimage1\').src=\'_libs/captchaget0.php?r=\'+Math.random()" href="javascript:void(0)">显示/更换验证码</a><br />';
         $this->div_preferences .= '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<input type="submit" name="PreferencesPersonal_2" value="修改密码" />';
         $this->div_preferences .= '</fieldset></form>';
 
